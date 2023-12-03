d1357401c3cbb5926a334d5f62c677f1de308fa6
rahulkalantre <rahulkalantre97@gmail.com>
2023-12-03 19:10:30 +0530

    [Navbar Responsive]
    Made a navbar responsive for mobile view.

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -2,39 +2,27 @@ import React, { useState } from "react";
 import { GoHome } from "react-icons/go";
 import { MdPodcasts } from "react-icons/md";
 import { FaHeadphonesAlt } from "react-icons/fa";
-import { BiUserCircle } from "react-icons/bi";
 import { BsSearch } from "react-icons/bs";
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button,
-} from "react-bootstrap";
+import { Navbar, Nav, Form, FormControl } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import UserIcon from "../UserIcon/UserIcon";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../NavigationBar/NavigationBar.css";
-
 const NavigationBar = () => {
-  // To change the color of nav after scrolling
   const [navicolor, setNaviColor] = useState(false);
   const [searchValue, setSearchValue] = useState("");
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
   const navigate = useNavigate();
   const localData = JSON.parse(localStorage.getItem("user-info"));
 
-  const changeColor = () => {
-    if (window.scrollY >= 10) {
-      setNaviColor(true);
-    } else {
-      setNaviColor(false);
-    }
-  };
-  window.addEventListener("scroll", changeColor);
-
+  // const changeColor = () => {
+  //   if (window.scrollY >= 10) {
+  //     setNaviColor(true);
+  //   } else {
+  //     setNaviColor(false);
+  //   }
+  // };
+  // window.addEventListener("scroll", changeColor);
+  
   const handleSearchChange = (e) => {
     setSearchValue(e.target.value);
     if (searchValue.trim() !== "") {
@@ -43,59 +31,45 @@ const NavigationBar = () => {
       navigate(`/search`);
     }
   };
-
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Collapse
-        className={
-          navicolor
-            ? "basic-navbar-nav basic-navbar-nav-bg"
-            : "basic-navbar-nav"
-        }
-      >
-        <div className="left-div">
-          <Navbar.Brand as={Link} to="/">
-            Amazon Music
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Nav className="mr-auto">
-            <Nav.Link as={Link} to="/">
-              <GoHome className="nav-icons" /> Home
-            </Nav.Link>
-            <Nav.Link
-              as={Link}
-              to={localData?.status === "success" ? `/podCasts` : `/aleartPage`}
-            >
-              <MdPodcasts className="nav-icons" /> Podcasts
-            </Nav.Link>
-            <Nav.Link as={Link} to="/library">
-              <FaHeadphonesAlt className="nav-icons" /> Library
-            </Nav.Link>
-          </Nav>
-        </div>
-        <div className="right-div">
+    <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+      <Navbar.Brand as={Link} to="/">
+        Amazon Music
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse className={navicolor ? "basic-navbar-nav-bg" : ""}>
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/">
+            <GoHome className="nav-icons" /> Home
+          </Nav.Link>
+          <Nav.Link
+            as={Link}
+            to={localData?.status === "success" ? `/podCasts` : `/aleartPage`}
+          >
+            <MdPodcasts className="nav-icons" /> Podcasts
+          </Nav.Link>
+          <Nav.Link as={Link} to="/library">
+            <FaHeadphonesAlt className="nav-icons" /> Library
+          </Nav.Link>
+        </Nav>
+        <div className="ml-auto d-flex align-items-center">
           <Link
             to={localData?.status === "success" ? `/search` : `/aleartPage`}
+            className="d-flex align-items-center"
           >
-            <Navbar.Collapse id="basic-navbar-nav">
-              <FormControl
-                type="text"
-                placeholder="Search"
-                className="mr-sm-2 search-input"
-                style={{ width: "200px" }}
-                value={searchValue}
-                onChange={handleSearchChange}
-              />
-              <BsSearch className="search-icon" />
-            </Navbar.Collapse>
+            <FormControl
+              type="text"
+              placeholder="Search"
+              className="mr-sm-2 search-input"
+              value={searchValue}
+              onChange={handleSearchChange}
+            />
+            <BsSearch className="search-icon" />
           </Link>
-          <div>
-            <UserIcon />
-          </div>
+          <UserIcon />
         </div>
       </Navbar.Collapse>
     </Navbar>
   );
 };
-
 export default NavigationBar;
