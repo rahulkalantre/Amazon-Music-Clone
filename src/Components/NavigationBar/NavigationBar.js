14788dc2b5d4c69c126af8a913ea64e54c4d698a
rahulkalantre <rahulkalantre97@gmail.com>
2024-01-05 14:29:20 +0530

    [ Responsive Navigation Bar ]
    - Changed the logo.
    - Recreated search bar functionality as per the reequipments.
    - Added condition to Navigation Bar for different screen sizes.

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -3,17 +3,17 @@ import { GoHome } from "react-icons/go";
 import { MdPodcasts } from "react-icons/md";
 import { FaHeadphonesAlt } from "react-icons/fa";
 import { BsSearch } from "react-icons/bs";
-import { Navbar, Nav, Form, FormControl } from "react-bootstrap";
+import { Navbar, Nav, Form, FormControl, NavLink } from "react-bootstrap";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import UserIcon from "../UserIcon/UserIcon";
-import amazonmusic from "../../Assets/amazonmusic";
-import logo from "../../Assets/Amazon-Music-Logo.png"
+import logo1 from "../../Assets/Amazon-Music-Logo.png";
+import logo2 from "../../Assets/Amazon-Music-Responsive-Logo.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../NavigationBar/NavigationBar.css";
-
 const NavigationBar = () => {
   const [searchValue, setSearchValue] = useState("");
   const [expanded, setExpanded] = useState(false);
+  const [searchField, setSearchField] = useState(true);
   const navigate = useNavigate();
   const location = useLocation();
   const localData = JSON.parse(localStorage.getItem("user-info"));
@@ -39,55 +39,139 @@ const NavigationBar = () => {
     } else {
       setExpanded((prevExpanded) => !prevExpanded);
     }
+    setSearchField(false);
   };
   return (
-    <Navbar
-      bg="dark"
-      variant="dark"
-      expand="lg"
-      fixed="top"
-      expanded={expanded}
-      onToggle={(isExpanded) => setExpanded(isExpanded)}
-    >
-      <Navbar.Brand as={Link} to="/" onClick={() => setExpanded(false)}>
-        <img src={logo} alt="Amazon-Music-Logo" className="amazon-music-logo" />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse>
-        <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/" onClick={() => setExpanded(false)}>
-            <GoHome className="nav-icons" /> <span className="nva-icon-text">HOME</span>
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to={localData?.status === "success" ? `/podCasts` : `/aleartPage`}
-            onClick={() => setExpanded(false)}
-          >
-            <MdPodcasts className="nav-icons" /> <span className="nva-icon-text">PODCASTS</span>
-          </Nav.Link>
-          <Nav.Link as={Link} to="/library" onClick={() => setExpanded(false)}>
-            <FaHeadphonesAlt className="nav-icons" /> <span className="nva-icon-text">LIBRARY</span>
-          </Nav.Link>
-        </Nav>
-        <div className="ml-auto d-flex align-items-center main-search">
-          <Form onSubmit={handleSearchSubmit}>
-            <FormControl
-              type="text"
-              placeholder="Search"
-              className="mr-sm-2 search-input"
-              value={searchValue}
-              onChange={handleSearchChange}
+    <>
+      {window.innerWidth <= 768 ? (
+        searchField ? (
+          <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+            <Navbar.Brand as={Link} to="/">
+              <img
+                src={logo2}
+                alt="Amazon-Music-Logo"
+                className="amazon-music-logo"
+              />
+            </Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link
+                className="linkA"
+                as={Link}
+                to="/"
+                onClick={() => setExpanded(false)}
+              >
+                <GoHome className="nav-icons" />
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to={
+                  localData?.status === "success" ? `/podCasts` : `/aleartPage`
+                }
+                onClick={() => setExpanded(false)}
+              >
+                <MdPodcasts className="nav-icons" />
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to="/library"
+                onClick={() => setExpanded(false)}
+              >
+                <FaHeadphonesAlt className="nav-icons" />
+              </Nav.Link>
+              <BsSearch
+                onClick={handleSearchIconClick}
+                className="nav-icons searchIcon"
+              />
+              <UserIcon />
+            </Nav>
+          </Navbar>
+        ) : (
+          <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+            <div className="resposive-search">
+              <div className = "resposive-search-bar">
+                <Form onSubmit={handleSearchSubmit}>
+                  <FormControl
+                    type="text"
+                    placeholder="Search"
+                    value={searchValue}
+                    onChange={handleSearchChange}
+                  />
+                </Form>
+              </div>
+              <Nav.Link as={Link} to="/">
+                <button
+                  className="cancel-button"
+                  onClick={() => setSearchField(true)}
+                >
+                  CANCEL
+                </button>
+              </Nav.Link>
+            </div>
+          </Navbar>
+        )
+      ) : (
+        <Navbar
+          bg="dark"
+          variant="dark"
+          expand="lg"
+          fixed="top"
+          expanded={expanded}
+          onToggle={(isExpanded) => setExpanded(isExpanded)}
+        >
+          <Navbar.Brand as={Link} to="/" onClick={() => setExpanded(false)}>
+            <img
+              src={logo1}
+              alt="Amazon-Music-Logo"
+              className="amazon-music-logo"
             />
-          </Form>
-          <BsSearch
-            id="search-icon"
-            className="search-icon"
-            onClick={handleSearchIconClick}
-          />
-          <UserIcon />
-        </div>
-      </Navbar.Collapse>
-    </Navbar>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse>
+            <Nav className="mr-auto">
+              <Nav.Link as={Link} to="/" onClick={() => setExpanded(false)}>
+                <GoHome className="nav-icons" />
+                <span className="nva-icon-text">HOME</span>
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to={
+                  localData?.status === "success" ? `/podCasts` : `/aleartPage`
+                }
+                onClick={() => setExpanded(false)}
+              >
+                <MdPodcasts className="nav-icons" />
+                <span className="nva-icon-text">PODCASTS</span>
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to="/library"
+                onClick={() => setExpanded(false)}
+              >
+                <FaHeadphonesAlt className="nav-icons" />
+                <span className="nva-icon-text">LIBRARY</span>
+              </Nav.Link>
+            </Nav>
+            <div className="ml-auto d-flex align-items-center main-search">
+              <Form onSubmit={handleSearchSubmit}>
+                <FormControl
+                  type="text"
+                  placeholder="Search"
+                  className="mr-sm-2 search-input"
+                  value={searchValue}
+                  onChange={handleSearchChange}
+                />
+              </Form>
+              <BsSearch
+                id="search-icon"
+                className="search-icon"
+                onClick={handleSearchIconClick}
+              />
+              <UserIcon />
+            </div>
+          </Navbar.Collapse>
+        </Navbar>
+      )}
+    </>
   );
 };
 export default NavigationBar;
